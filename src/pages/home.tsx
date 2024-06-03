31b94dab7fa8e2fc888f31a82a465a289c227a7d
dev-rathore <rathoredvd3001@gmail.com>
2024-06-03 21:59:52 +0530

    integrated everything together

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,27 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import WeatherDisplay from '@/components/ui/weather-display/weather-display.component';
-import MapDisplay from '@/components/ui/map-display/map-display.component';
+import React, { useState } from 'react';
+import WeatherDisplay from '@/components/ui/weather-display/weather-display';
+import MapDisplay from '@/components/ui/map-display/map-display';
 import SearchBar from '@/components/ui/search-bar';
+import { useWeatherContext } from '@/contexts/weather.provider';
+import WeatherDataList from '@/components/ui/weather-data-list/weather-data-list';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 const Home: React.FC = () => {
+  const [zoom] = useState<number>(13);
   const [lat, setLat] = useState<number>(51.505);
   const [lon, setLon] = useState<number>(-0.09);
-  const [zoom] = useState<number>(13);
-  const [searchHistory, setSearchHistory] = useState<Array<{ lat: number, lon: number }>>([]);
+  const {
+    weatherDataList,
+    currentLocationWeatherData,
+    saveWeatherData,
+  } = useWeatherContext();
 
-  useEffect(() => {
-    const storedHistory = JSON.parse(localStorage.getItem('searchHistory') || '[]');
-    setSearchHistory(storedHistory);
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
-  }, [searchHistory]);
-
-  const handleSetLatLon = (latitude: number, longitude: number) => {
+  const onLatLngChange = async (latitude: number, longitude: number) => {
     setLat(latitude);
     setLon(longitude);
-    setSearchHistory([{ lat: latitude, lon: longitude }, ...searchHistory.slice(0, 4)]);
+    await saveWeatherData(latitude, longitude);
   };
 
   const handleSubmit = (event: React.FormEvent) => {
@@ -29,50 +28,46 @@ const Home: React.FC = () => {
     const form = event.target as HTMLFormElement;
     const latInput = form.elements.namedItem('lat') as HTMLInputElement;
     const lonInput = form.elements.namedItem('lon') as HTMLInputElement;
-    handleSetLatLon(parseFloat(latInput.value), parseFloat(lonInput.value));
+    onLatLngChange(parseFloat(latInput.value), parseFloat(lonInput.value));
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">Weather and Map Application</h1>
-      <form onSubmit={handleSubmit} className="mb-4">
-        <div className="flex space-x-2 mb-2">
-          <input
-            type="number"
-            name="lat"
-            placeholder="Latitude"
-            required
-            className="p-2 border rounded w-full"
+    <div className="container mx-auto px-4 py-6 flex flex-col gap-4 min-h-screen">
+      <h1 className="text-3xl font-bold text-center mb-4">Weather Web App</h1>
+      <div className='grow flex flex-col md:flex-row gap-4'>
+        <div className='flex flex-col sm:flex-row md:flex-col gap-4 order-2 md:order-1'>
+          {
+            currentLocationWeatherData &&
+              <WeatherDisplay
+                weather={currentLocationWeatherData}
+              />
+          }
+          <WeatherDataList
+            weatherDataList={weatherDataList}
           />
-          <input
-            type="number"
-            name="lon"
-            placeholder="Longitude"
-            required
-            className="p-2 border rounded w-full"
-          />
-          <button type="submit" className="p-2 bg-blue-500 text-white rounded">Submit</button>
         </div>
-        <SearchBar setLatLon={handleSetLatLon} />
-      </form>
-      <MapDisplay lat={lat} lon={lon} zoom={zoom} setLatLon={handleSetLatLon} />
-      <WeatherDisplay lat={lat} lon={lon} />
-      {searchHistory.length > 0 && (
-        <div className="mt-4">
-          <h2 className="text-xl font-bold">Search History</h2>
-          <ul>
-            {searchHistory.map((location, index) => (
-              <li
-                key={index}
-                onClick={() => handleSetLatLon(location.lat, location.lon)}
-                className="cursor-pointer underline text-blue-500"
-              >
-                {`Lat: ${location.lat}, Lon: ${location.lon}`}
-              </li>
-            ))}
-          </ul>
+        <div className='flex flex-col grow gap-4 order-1 md:order-2'>
+          <form onSubmit={handleSubmit}>
+            <div className="flex space-x-2 mb-4">
+              <Input
+                type="number"
+                name="lat"
+                placeholder="Latitude"
+                required
+              />
+              <Input
+                type="number"
+                name="lon"
+                placeholder="Longitude"
+                required
+              />
+              <Button type="submit" size="lg">SUBMIT</Button>
+            </div>
+            <SearchBar setLatLon={onLatLngChange} />
+          </form>
+          {lat && lon && <MapDisplay lat={lat} lon={lon} zoom={zoom} setLatLon={onLatLngChange} />}
         </div>
-      )}
+      </div>
     </div>
   );
 };
